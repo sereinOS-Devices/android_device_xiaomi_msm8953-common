e184e7cffedc7fec8d0286fbe93f584b7bdb62a3
Ethan Chen <intervigil@gmail.com>
2017-09-16 22:14:36 -0700

    msm8953-common: Add basic USB HAL that reports no status change
    - Changed name from basic to xiaomi to avoid conflicts with other
    devices.
    
    Change-Id: I38bfe869f9b1b66db0e13249f65e438878d06eff
    Signed-off-by: RahifM <faizel326@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -331,7 +331,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi
 
 # VNDK
 PRODUCT_PACKAGES += \
